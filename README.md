ecc3ccb990a6f5ae868e19667b9c25107a6aceab
Wes Higbee <wes.mcclure@gmail.com>
2016-10-31 10:12:35 -0400

    README
    README

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # docker-static-binary
+
 An example of baking a static linked linux binary into a container alone with nothing else
+
+To run this demo execute `./demo.sh` and follow along with the output
+
+Check the size of the resultant docker image:
+`docker images static-run`
+
+Cleanup:
+```bash
+docker rmi static-build static-run
+```
--- demo.sh
+++ demo.sh
@@ -1,9 +1,14 @@
+#!/bin/bash
+
+# Remove previous build output
+rm run/hello
+
 # First we need to build the static linked binary, we do this in an alpine linux container with g++
 echo 1. Build our build environment:
 docker build -f build/Dockerfile build -t static-build
 echo -e "\n"
 echo 2. Run our build environment, to produce a static linked binary:
-docker run -v $(pwd):/workdir -w /workdir static-build g++ -static build/hello.cpp -o run/hello
+docker run --rm -v $(pwd):/workdir -w /workdir static-build g++ -static build/hello.cpp -o run/hello
 
 echo -e "\n"
 echo 3. Build our runtime environment: just copy the static linked binary in a container image:
@@ -11,4 +16,4 @@ docker build -f run/Dockerfile run -t static-run
 
 echo -e "\n"
 echo 4. Run our runtime environment:
-docker run static-run
+docker run --rm static-run
