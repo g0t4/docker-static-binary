6c2be03fb1b6cbb48cfc63a987eb8025caa6aaf4
Wes Higbee <wes.mcclure@gmail.com>
2016-11-01 12:27:25 -0400

    Switch to c - standalone executable is much smaller

--- demo.sh
+++ demo.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-# Remove previous build output
+echo 0. Remove existing run/hello binary
 rm run/hello
 
-# First we need to build the static linked binary, we do this in an alpine linux container with g++
 echo 1. Build our build environment:
 docker build -f build/Dockerfile build -t static-build
 echo -e "\n"
-echo 2. Run our build environment, to produce a static linked binary:
-docker run --rm -v $(pwd):/workdir -w /workdir static-build g++ -static build/hello.cpp -o run/hello
+echo 2. Run our build environment, to produce a standalone executable:
+docker run --rm -v $(pwd):/workdir -w /workdir static-build gcc -static build/hello.c -o run/hello
 
 echo -e "\n"
 echo 3. Build our runtime environment: just copy the static linked binary in a container image:
@@ -17,3 +16,7 @@ docker build -f run/Dockerfile run -t static-run
 echo -e "\n"
 echo 4. Run our runtime environment:
 docker run --rm static-run
+
+echo -e "\n" 
+echo 5. Binary size:
+ls -lh run/hello
